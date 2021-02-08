c41499ff7862fe1667d2f9461195a90c214520fd
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-08 10:18:03 +0100

    Hero section added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,42 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const data = useStaticQuery(query)
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = data
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div className="underline"></div>
+          <h1>I'm John</h1>
+          <h4>Freelance Web and Mobile UI/IX Designer</h4>
+          <Link className="btn" to="/contact">
+            Contact me
+          </Link>
+          <SocialLinks />
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
