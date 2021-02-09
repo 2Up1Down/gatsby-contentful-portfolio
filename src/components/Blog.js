66e809edc68aa7d348e1fae45d48f8d97f6103f5
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-09 21:16:58 +0100

    Changed props destructuring from Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,21 +3,14 @@ import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { Link } from "gatsby"
 const Blog = ({ ...blog }) => {
-  const {
-    category,
-    date,
-    description,
-    image: {
-      childImageSharp: { fluid: image },
-    },
-    slug,
-    title,
-  } = blog
+  const { category, date, description, image, slug, title } = blog
 
   return (
     <Link to={`/blog/${slug}`}>
       <article className="blog">
-        <Image className="blog-img" fluid={image} />
+        {image && (
+          <Image className="blog-img" fluid={image.childImageSharp.fluid} />
+        )}
         <div className="blog-card">
           <h4>{title}</h4>
           <p>{description}</p>
@@ -31,6 +24,13 @@ const Blog = ({ ...blog }) => {
   )
 }
 
-Blog.propTypes = {}
+Blog.propTypes = {
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  slug: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
 
 export default Blog
