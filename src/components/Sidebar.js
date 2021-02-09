962602f87208b1e32c2e44949dd0a567e97350fa
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-09 22:47:43 +0100

    First draft for Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const [sidebarVisibility, setSidebarVisibility] = useState(true)
+
+  return (
+    <aside className={`sidebar ${sidebarVisibility ? "show-sidebar" : ""}`}>
+      <button
+        className="close-btn"
+        onClick={() => setSidebarVisibility(!sidebarVisibility)}
+      >
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
