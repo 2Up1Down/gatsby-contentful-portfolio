1ee032ca4a85412c0e468a151564066325216026
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-09 18:34:40 +0100

    Dynamic pages for blog added

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,8 +3,28 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
 
-const ComponentName = () => {
-  return <h2>blog post template</h2>
+const blogTemplate = ({ data }) => {
+  console.log(data)
+  const {
+    blog: { category, content, date, title },
+  } = data
+
+  console.log(content)
+  return (
+    <Layout>
+      <section className="blog-template">
+        <div className="section-center">
+          <article className="blog-content">
+            <h1>{title}</h1>
+            <ReactMarkdown children={content} />
+            <Link to="/blog" className="btn center-btn">
+              Blog
+            </Link>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 // export const query = graphql`
@@ -16,4 +36,24 @@ const ComponentName = () => {
 //   }
 // `
 
-export default ComponentName
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      category
+      content
+      date
+      id
+      slug
+      title
+      image {
+        childImageSharp {
+          fixed {
+            src
+          }
+        }
+      }
+    }
+  }
+`
+
+export default blogTemplate
