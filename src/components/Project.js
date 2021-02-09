2639ce7e75296fb901c72c6eba5cecb5bab4d604
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-09 20:59:54 +0100

    Added PropTypes to Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -40,6 +40,13 @@ const Project = ({ index, ...rest }) => {
   )
 }
 
-Project.propTypes = {}
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  github: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  stack: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
 
 export default Project
