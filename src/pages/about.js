1c7c5f328d982dc653978ddda5ef011d432fa463
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-10 18:22:30 +0100

    Added about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,56 @@
 import React from "react"
-import Layout from "../components/Layout"
+import ReactMarkdown from "react-markdown"
 import { graphql } from "gatsby"
-import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+import Layout from "../components/Layout"
+import Title from "../components/Title"
+
+const About = ({ data }) => {
+  const {
+    about: { description, image, stack, title },
+  } = data
+
+  console.log(description)
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <ReactMarkdown children={description} />
+            <div className="about-stack">
+              {stack.map(item => (
+                <span key={item.id}>{item.name}</span>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    about: strapiAbout {
+      id
+      description
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      stack {
+        name
+        id
+      }
+      title
+    }
+  }
+`
 
 export default About
