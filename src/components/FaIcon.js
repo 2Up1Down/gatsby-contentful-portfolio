e1866ef8456e68c51a51e3d0f4dcd2665487c6de
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-11 19:34:49 +0100

    Replaced div with span for inline

--- src/components/FaIcon.js
+++ src/components/FaIcon.js
@@ -4,7 +4,7 @@ import * as FontAwesome from "react-icons/fa"
 const FaIcon = ({ iconName, className }) => {
   const icon = React.createElement(FontAwesome[iconName])
   // return <div style={{ fontSize: size, color: color }}>{icon}</div>
-  return <div className={className}>{icon}</div>
+  return <span className={className}>{icon}</span>
 }
 
 export default FaIcon
