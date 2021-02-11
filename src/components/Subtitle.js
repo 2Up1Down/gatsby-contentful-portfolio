377e1f0f08446fe8ca62615c242f5425a129d03a
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-11 19:35:24 +0100

    Fixed naming of default subtitle

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -3,7 +3,7 @@ import React from "react"
 const Subtitle = ({ subtitle }) => {
   return (
     <div className="section-title">
-      <h3>{subtitle || "default title"}</h3>
+      <h3>{subtitle || "default subtitle"}</h3>
     </div>
   )
 }
