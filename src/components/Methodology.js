14239622c5ad9da9d698934df82a7a01bf66c34f
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-12 15:10:31 +0100

    Used wrong Image fragment

--- src/components/Methodology.js
+++ src/components/Methodology.js
@@ -14,7 +14,7 @@ const query = graphql`
         id
         image {
           fluid {
-            ...GatsbyContentfulFluid_tracedSVG
+            ...GatsbyContentfulFluid
           }
         }
         title
