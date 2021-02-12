b9a7dcaabe6e3b5a8afb1b2d7b52b9fd80fc572f
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-12 15:46:27 +0100

    Not needed currently, might be completly deleted

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,61 +1,52 @@
-import React from "react"
-import ReactMarkdown from "react-markdown"
-import { graphql, Link } from "gatsby"
+// import React from "react"
+// import ReactMarkdown from "react-markdown"
+// import { graphql, Link } from "gatsby"
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+// import Layout from "../components/Layout"
+// import SEO from "../components/SEO"
 
-const blogTemplate = ({ data }) => {
-  const {
-    blog: { category, content, date, description, title },
-  } = data
+// const blogTemplate = ({ data }) => {
+//   const {
+//     blog: { category, content, date, description, title },
+//   } = data
 
-  return (
-    <Layout>
-      <SEO title={title} description={description} />
-      <section className="blog-template">
-        <div className="section-center">
-          <article className="blog-content">
-            <h1>{title}</h1>
-            <ReactMarkdown children={content} />
-            <Link to="/blog" className="btn center-btn">
-              Blog
-            </Link>
-          </article>
-        </div>
-      </section>
-    </Layout>
-  )
-}
+//   return (
+//     <Layout>
+//       <SEO title={title} description={description} />
+//       <section className="blog-template">
+//         <div className="section-center">
+//           <article className="blog-content">
+//             <h1>{title}</h1>
+//             <ReactMarkdown children={content} />
+//             <Link to="/blog" className="btn center-btn">
+//               Blog
+//             </Link>
+//           </article>
+//         </div>
+//       </section>
+//     </Layout>
+//   )
+// }
 
 // export const query = graphql`
 //   query GetSingleBlog($slug: String) {
 //     blog: strapiBlogs(slug: { eq: $slug }) {
+//       category
 //       content
-
+//       date
+//       description
+//       id
+//       slug
+//       title
+//       image {
+//         childImageSharp {
+//           fixed {
+//             src
+//           }
+//         }
+//       }
 //     }
 //   }
 // `
 
-export const query = graphql`
-  query GetSingleBlog($slug: String) {
-    blog: strapiBlogs(slug: { eq: $slug }) {
-      category
-      content
-      date
-      description
-      id
-      slug
-      title
-      image {
-        childImageSharp {
-          fixed {
-            src
-          }
-        }
-      }
-    }
-  }
-`
-
-export default blogTemplate
+// export default blogTemplate
