c118048f1a85b6ac0ffa2ea77dabd91ff4c2a2eb
2Up1Down <39255639+2Up1Down@users.noreply.github.com>
2021-02-17 20:34:25 +0100

    Style optimized for mobile devices

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -3,8 +3,10 @@ import React from "react"
 const Citation = () => {
   return (
     <section className="section citation">
-      <q className="citation-quote">Keep it simple, stupid</q>
-      <p className="citation-author">KISS princible by Kelly Johnson</p>
+      <div className="section-center citation-center">
+        <q className="citation-quote">Keep it simple, stupid</q>
+        <p className="citation-author">KISS princible by Kelly Johnson</p>
+      </div>
     </section>
   )
 }
